e0302efb31a984c12f58e8dfa8957cc7ac22b3f1
piratejas <piratejas@gmail.com>
2023-11-28 15:27:24 +0000

    implemented basic post to auth/register backend endpoint; TODO: form validation and refactor

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+
+router.use(express.urlencoded({ extended: true }));
 
 router.route("/login")
     .get((req, res, next) => {
@@ -14,9 +17,46 @@ router.route("/registration")
     .get((req, res, next) => {
         res.render('user/registration.njk');
     })
-    .post((req, res, next) => {
-        // TODO: form handler middleware
-        res.redirect("/user/confirmation");
+    .post(async (req, res, next) => {
+		console.log(req.body);
+        // const username = req.body.name;
+        // const city = req.body.city;
+        // const county = req.body.county;
+        // const postCode = req.body.postCode;
+		// const dairyAllergy = "dairyAllergy" in req.body;
+        // const eggAllergy = "eggAllergy" in req.body;
+		// const peanutAllergy = "peanutAllergy" in req.body;
+        // const password = req.body.password;
+        // const confirmPassword = req.body.confirmpassword;
+
+		const data = {
+			"username": req.body.name,
+			"password": req.body.password,
+			"city": req.body.city,
+			"county": req.body.county,
+			"postCode": req.body.postCode,
+			"dairyAllergy": "dairyAllergy" in req.body,
+			"eggAllergy": "eggAllergy" in req.body,
+			"peanutAllergy": "peanutAllergy" in req.body
+		}
+
+		try {
+            // Make a POST request to the external server
+            const response = await axios.post('http://localhost:4001/auth/register', data);
+
+            // Assuming the external server responds with a success status
+            if (response.status === 200) {
+                // Redirect to confirmation page after successful registration
+                res.redirect("/user/confirmation");
+            } else {
+                // Handle other response statuses as needed
+                res.status(response.status).send('Registration failed');
+            }
+        } catch (error) {
+            // Handle errors from the POST request
+            console.error('Error sending POST request:', error);
+            res.status(500).send('Internal Server Error');
+        }
     })
 
 router.get("/confirmation", (req, res, next) => {
