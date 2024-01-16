b1c0c50651aee7bdcc47002f03703c3e333abfce
piratejas <piratejas@gmail.com>
2024-01-16 16:45:14 +0000

    changed hrefs in header; added story template and route

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,6 +9,10 @@ router.get("/documentation", (req, res, next) => {
     res.render('documentation.njk');
 })
 
+router.get("/story", (req, res, next) => {
+    res.render('story.njk');
+})
+
 router.get("/internalServerError", (req, res, next) => {
     res.render('internalServerError.njk');
 })
